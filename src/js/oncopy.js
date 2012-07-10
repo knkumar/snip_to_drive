62f418754edf137662451d804b7d2d305e351129
Kiran <krankumar@gmail.com>
2012-07-10 03:26:21 -0400

    landing changes

--- src/js/oncopy.js
+++ src/js/oncopy.js
@@ -5,7 +5,9 @@
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     if (request.method == "getSelection") {
-	sendResponse({ data: getSelectionHTML() });
+	var selection = getSelectionHTML();
+	//alert("selection: "+selection);
+	sendResponse({ data: selection? selection : document.execCommand('paste'); });
     }
     else
       sendResponse({}); // snub them.
